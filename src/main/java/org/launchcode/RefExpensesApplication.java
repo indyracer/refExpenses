f5febbf28aba400c33897ab557805aa45c2b20b5
Bob Cohen <bobcohen1969@yahoo.com>
2017-02-13 11:04:34 -0600

    starting new project

---
+++ src/main/java/org/launchcode/RefExpensesApplication.java
@@ -0,0 +1,12 @@
+package org.launchcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefExpensesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefExpensesApplication.class, args);
+	}
+}
